c4749abbeb817acef0a9526c8bb971a95c0db970
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2017-07-18 17:56:23 +0900

    Support page list search

--- attributes/express_multiple/controller.php
+++ attributes/express_multiple/controller.php
@@ -2,11 +2,15 @@
 
 namespace Concrete\Package\MultipleExpressEntrySelector\Attribute\ExpressMultiple;
 
+use Concrete\Core\Entity\Attribute\Value\Value\ExpressValue;
 use Concrete\Core\Entity\Express\Entity;
 use Concrete\Core\Entity\Express\Entry;
+use Concrete\Core\Search\ItemList\Database\AttributedItemList;
 
 class Controller extends \Concrete\Attribute\Express\Controller
 {
+    protected $searchIndexFieldDefinition = ['type' => 'text', 'options' => ['default' => null, 'notnull' => false]];
+
     public function form()
     {
         $entryOptions = [];
@@ -50,5 +54,43 @@ class Controller extends \Concrete\Attribute\Express\Controller
         }
 
         return $this->createAttributeValue($entries);
+    }
+
+    public function getSearchIndexValue()
+    {
+        $str = "||";
+        $o = $this->attributeValue;
+        if (is_object($o)) {
+            $entries = $o->getValue()->getSelectedEntries();
+            if (count($entries) > 0) {
+                foreach ($entries as $entry) {
+                    $str .= $entry->getLabel() . "||";
+                }
+            }
+        }
+        // remove line break for empty list
+        if ($str == "||") {
+            return '';
+        }
+
+        return $str;
+    }
+
+    public function filterByAttribute(AttributedItemList $list, $value, $comparison = '=')
+    {
+        if ($value instanceof ExpressValue) {
+            /** @var \Doctrine\DBAL\Query\QueryBuilder $qb */
+            $qb = $list->getQueryObject();
+            $i = 1;
+            $expressions = [];
+            foreach ($value->getSelectedEntries() as $entry) {
+                $column = 'ak_' . $this->attributeKey->getAttributeKeyHandle();
+                $expressions[] = $qb->expr()->like($column, ':entryLabel' . $i);
+                $qb->setParameter('entryLabel' . $i, "%||" . $entry->getLabel() . '||%');
+                ++$i;
+            }
+            $expr = $qb->expr();
+            $qb->andWhere(call_user_func_array([$expr, 'orX'], $expressions));
+        }
     }
 }
