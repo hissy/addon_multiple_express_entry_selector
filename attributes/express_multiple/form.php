7b561b04904bad5b9510168f9592157364a6b980
Dimitris Gerasopoulos <dimgera84@hotmail.com>
2020-10-14 14:07:06 +0200

    Make input selector more spesific

--- attributes/express_multiple/form.php
+++ attributes/express_multiple/form.php
@@ -1,12 +1,13 @@
 <?php
 defined('C5_EXECUTE') or die("Access Denied.");
 /** @var \Concrete\Core\Entity\Express\Entity $entity */
+/** @var integer $attributeKeyID */
 ?>
-<input type="hidden" data-selectable="<?= $entity->getId() ?>" style="width: 100%"
+<input type="hidden" data-selectable="<?= $entity->getId().'-ak-'.$attributeKeyID ?>" style="width: 100%"
        name="<?= $view->field('value') ?>" value="<?= $value ?>"/>
 <script type="text/javascript">
     $(function() {
-        $('input[data-selectable=<?= $entity->getId() ?>]').selectize({
+        $('input[data-selectable=<?= $entity->getId().'-ak-'.$attributeKeyID ?>]').selectize({
             plugins: ['remove_button'],
             valueField: 'id',
             labelField: 'text',
